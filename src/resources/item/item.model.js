f857b3fc51ab28aaace3b1e2a169e5b79a25f33a
Eneko Bikandi <eneko.bikandi@ve.com>
2020-05-01 19:20:18 +0200

    [Items]: cretes schema

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,32 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50
+    },
+    status: {
+      default: 'active',
+      required: true,
+      type: String,
+      enum: ['active', 'complete', 'pastdue']
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'user',
+      required: true
+    },
+    list: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'list',
+      required: true
+    }
+  },
+  { timestamps: true }
+)
 export const Item = mongoose.model('item', itemSchema)
