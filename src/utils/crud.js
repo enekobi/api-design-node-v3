72d73d5b6d6cbd507dcdfcb12233af8cb6cad991
Eneko Bikandi <eneko.bikandi@ve.com>
2020-05-01 20:43:59 +0200

    crud methods

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,62 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
 
-export const getMany = model => async (req, res) => {}
+  const doc = await model.findOne({ _id: id, createdBy: userId }).exec()
 
-export const createOne = model => async (req, res) => {}
+  if (!doc) {
+    return res.status(404).end()
+  }
 
-export const updateOne = model => async (req, res) => {}
+  res.status(200).json({ data: doc })
+}
 
-export const removeOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const userId = req.user._id
+
+  const docs = await model.find({ createdBy: userId })
+
+  res.status(200).json({ data: docs })
+}
+
+export const createOne = model => async (req, res) => {
+  const userId = req.user._id
+
+  const doc = await model.create({ ...req.body, createdBy: userId })
+
+  res.status(201).json({ data: doc })
+}
+
+export const updateOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+  const body = req.body
+
+  const doc = await model
+    .findOneAndUpdate({ _id: id, createdBy: userId }, body, {
+      new: true
+    })
+    .exec()
+
+  if (!doc) {
+    return res.status(400).end()
+  }
+  res.status(200).json({ data: doc })
+}
+
+export const removeOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+
+  const doc = await model
+    .findOneAndRemove({ _id: id, createdBy: userId })
+    .exec()
+
+  if (!doc) {
+    return res.status(400).end()
+  }
+  res.status(200).json({ data: doc })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
